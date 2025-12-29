94b04426345a3d7fe0d95ced4840a758ae52b9f6
liangjs <761232680@qq.com>
2025-12-29 16:08:28 +0800

    support more c/c++ variable definition

--- coderetrx/static/codebase/queries/treesitter/tags/c.scm
+++ coderetrx/static/codebase/queries/treesitter/tags/c.scm
@@ -1,3 +1,5 @@
+; C language tags query - simplified version
+
 (struct_specifier name: (type_identifier) @name.definition.class body:(_)) @definition.class
 
 (declaration type: (union_specifier name: (type_identifier) @name.definition.class)) @definition.class
@@ -17,14 +19,50 @@
 
 (preproc_include) @import
 
-; Variable definitions - all declarations
+; Variable definitions - hybrid approach
+; Combines universal wildcard matching with specific function pointer patterns
+
+; Level 0: Direct identifier
 (declaration
   declarator: (identifier) @name.definition.variable) @definition.variable
 
+; Level 1: One level of nesting (covers most cases)
 (declaration
-  declarator: (init_declarator
+  declarator: (_
     declarator: (identifier) @name.definition.variable)) @definition.variable
 
+; Level 2: Two levels of nesting
 (declaration
-  declarator: (pointer_declarator
-    declarator: (identifier) @name.definition.variable)) @definition.variable
+  declarator: (_
+    declarator: (_
+      declarator: (identifier) @name.definition.variable))) @definition.variable
+
+; Level 3: Three levels of nesting
+(declaration
+  declarator: (_
+    declarator: (_
+      declarator: (_
+        declarator: (identifier) @name.definition.variable)))) @definition.variable
+
+; Level 4: Four levels of nesting
+(declaration
+  declarator: (_
+    declarator: (_
+      declarator: (_
+        declarator: (_
+          declarator: (identifier) @name.definition.variable))))) @definition.variable
+
+; Special case: Function pointers with parenthesized_declarator
+; These need explicit handling because function_declarator is at the top level
+(declaration
+  declarator: (function_declarator
+    declarator: (parenthesized_declarator
+      (_
+        declarator: (identifier) @name.definition.variable)))) @definition.variable
+
+(declaration
+  declarator: (function_declarator
+    declarator: (parenthesized_declarator
+      (_
+        declarator: (_
+          declarator: (identifier) @name.definition.variable))))) @definition.variable
